418fb53ffaf53608ae86d1216943da7d444ea2f1
marselleze <marselleze.88@gmail.com>
2024-06-18 12:26:40 +0300

    domain model has been added

---
+++ app/src/main/java/smg/mironov/ksuschedule/Model/Teacher.java
@@ -0,0 +1,33 @@
+package smg.mironov.ksuschedule.Model;
+
+
+public class Teacher {
+    private int id;
+    private String name;
+    private String post;
+
+    //Getter and setters
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPost(){
+        return post;
+    }
+
+    public void setPost(String post){
+        this.post = post;
+    }
+}
