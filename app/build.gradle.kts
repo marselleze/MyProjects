eec8db827ea708cc46495aa6cf5029ca9bdde0ac
marselleze <marselleze.88@gmail.com>
2024-06-18 11:51:16 +0300

    settings and teachers info screen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,13 @@ android {
 
 dependencies {
 
+    implementation ("androidx.appcompat:appcompat:1.3.0-beta01")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    //The rest of the dependencies here
+    // Kotlin
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
